72a8411890207e9abf6fafa8349c85fad5a3f988
Sebastian <sebastian.dziula@gmail.com>
2017-07-12 21:48:57 +0200

    doc

--- main.php
+++ main.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <title>Document</title>
 </head>
-<body>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur odit, aliquam eveniet nam a dignissimos commodi voluptas, adipisci fugit harum molestiae. Dignissimos dicta, accusantium provident autem, saepe vel quaerat totam?</p>
 </body>
 </html>
